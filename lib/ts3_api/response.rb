16ed018556906380d86cc92045f4f97ee223095c
simplay <silent.simplay@gmail.com>
2017-11-13 15:33:31 +0100

    Fetch all responses and not just the last one

--- lib/ts3_api/response.rb
+++ lib/ts3_api/response.rb
@@ -1,24 +1,47 @@
 module TS3API
   class Response
+    class Status
+      attr_reader :id, :msg
+      def initialize(msg)
+        require 'pry'; binding.pry if msg.nil?
+        raw_id, raw_status = msg.strip.split(/\s/)[1..-1]
+        @id = raw_id.delete('id=').to_i
+        @msg = raw_status.delete('msg=')
+      end
+
+      def ok?
+        msg == 'ok'
+      end
+    end
+
     attr_reader :message,
-                :attributes
+                :attributes,
+                :status
 
     def initialize(message)
       @message = message
-      splits = message.split(/\s/).reject(&:empty?)
-      @attributes = splits.map do |split|
-        key, value = split.split('=')
-        v = Decoder.new(value).decode
-        [key.to_sym, v || nil]
-      end.to_h
-    end
 
-    def status
-      attributes[:msg]
+      split_msg = message.split("\n")
+      case split_msg.count
+      when 1
+        @status = Status.new(split_msg.first)
+        @attributes = []
+      else
+        response_msg, status_msg = message.split("\n")
+        @status = Status.new(status_msg)
+        @attributes = response_msg.split('|').map do |r|
+          splits = r.split(/\s/).reject(&:empty?)
+          splits.map do |split|
+            key, value = split.split('=')
+            v = Decoder.new(value).decode
+            [key.to_sym, v || nil]
+          end.to_h
+        end
+      end
     end
 
     def ok?
-      status == "ok"
+      status.ok?
     end
   end
 end
